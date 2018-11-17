08bc5213d8c36deb787094c47c55b7929d541fb1
Glenn Wedin <glenn.wedin@gmail.com>
2018-11-17 03:26:53 +0100

    categories

--- intents/welcome.js
+++ intents/welcome.js
@@ -1,5 +1,12 @@
 module.exports = function(agent) {
-    const categories = ['music', 'sport', 'history', 'computers'];
+    const categories = [
+        'music',
+        'sport',
+        'history',
+        'computers',
+        'film',
+        'geography'
+    ];
     agent.add(
         `So you are ready to be challenged? Which category do you want to be quizzed about? ${categories
             .slice(0, -1)
