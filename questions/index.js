99144d575b0a92f85c4592dfc9b6d01a26f872d1
Glenn Wedin <glenn.wedin@gmail.com>
2018-11-17 03:23:25 +0100

    added questions

--- questions/index.js
+++ questions/index.js
@@ -2,4 +2,6 @@ const music = require('./music.json');
 const sport = require('./sport.json');
 const computers = require('./computers.json');
 const history = require('./history.json');
-module.exports = { music, sport, computers, history };
+const film = require('./film.json');
+const geography = require('./geography.json');
+module.exports = { music, sport, computers, history, film, geography };
